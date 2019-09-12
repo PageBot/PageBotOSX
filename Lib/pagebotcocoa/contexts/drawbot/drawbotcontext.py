7fa67e2cfa9324d7695817b277a00946c1a57c30
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-09-12 09:37:07 +0200

    Removing unused import.

--- Lib/pagebotcocoa/contexts/drawbot/drawbotcontext.py
+++ Lib/pagebotcocoa/contexts/drawbot/drawbotcontext.py
@@ -5,7 +5,6 @@
 #
 
 import os
-import traceback
 from AppKit import NSFont
 from CoreText import (CTFontDescriptorCreateWithNameAndSize,
         CTFontDescriptorCopyAttribute, kCTFontURLAttribute)
