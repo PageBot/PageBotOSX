c7fc46312fb713bdbfa08b4b2f582aea21ed8d1f
Michiel <michiel@kauwatjoe.com>
2020-03-03 12:13:37 +0100

    Moving vanilla-importing code to the Apps repository.

--- scripts/doctests.sh
+++ scripts/doctests.sh
@@ -4,13 +4,6 @@ export PWD="Lib/pagebotcocoa"
 python3 $PWD/__init__.py
 python3 $PWD/cocoacolor.py
 python3 $PWD/errors.py
-python3 $PWD/apps/__init__.py
-python3 $PWD/apps/baseapp.py
-python3 $PWD/apps/canvasapp.py
-python3 $PWD/apps/canvas/__init__.py
-python3 $PWD/apps/canvas/canvas.py
-python3 $PWD/apps/canvas/canvasview.py
-#- python3 $PWD/apps/pagebotdemoapp.py
 python3 $PWD/bezierpaths/__init__.py
 python3 $PWD/bezierpaths/cocoabezierpath.py
 python3 $PWD/bezierpaths/beziercontour.py
@@ -23,10 +16,6 @@ python3 $PWD/contexts/canvas/canvascontext.py
 python3 $PWD/contexts/canvas/canvasbuilder.py
 python3 $PWD/graphics/__init__.py
 python3 $PWD/graphics/graphic.py
-python3 $PWD/elements/__init__.py
-python3 $PWD/elements/variablefonts/__init__.py
-python3 $PWD/elements/variablefonts/variableeditor.py
-python3 $PWD/elements/variablefonts/opszglassbanner.py
 python3 $PWD/fonttoolbox/__init__.py
 python3 $PWD/fonttoolbox/svg2drawbot.py
 python3 $PWD/strings/__init__.py
